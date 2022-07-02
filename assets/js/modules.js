89a9fcd4dfa219858cd7f487483d04a759d10875
Firuz Hacıyev <firuz.haciyeff@gmail.com>
2022-07-02 23:54:06 +0400

    image collection completed

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -8,3 +8,22 @@ document.querySelectorAll("[data-toggle='collapse']").forEach((item) => {
     }
   });
 });
+
+document.querySelectorAll("[data-toggle='tab']").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll("[data-toggle='tab']").forEach((i) => {
+      const { target, activeClass } = i.dataset;
+      i.classList.remove(activeClass || "active");
+      if (document.querySelector(target)) {
+        document.querySelector(target).classList.remove("tab__pane--active");
+      }
+    });
+
+    const { target, activeClass } = item.dataset;
+    const targetItem = document.querySelector(target);
+    item.classList.add(activeClass || "active");
+    if (targetItem) {
+      targetItem.classList.add("tab__pane--active");
+    }
+  });
+});
